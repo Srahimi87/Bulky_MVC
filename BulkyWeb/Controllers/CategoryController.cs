77d0bd01d33360b5d6465689744f01c122eb2922
Sahar Rahimi <sahar.rahimi@corethree.net>
2023-11-28 09:43:56 +0000

    update edit and delete button

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -1,6 +1,7 @@
 ﻿using BulkyWeb.Data;
 using BulkyWeb.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 
 namespace BulkyWeb.Controllers
 {
@@ -23,9 +24,51 @@ namespace BulkyWeb.Controllers
         [HttpPost]
         public IActionResult Create(Category obj)
         {
-            _db.Categories.Add(obj);
-            _db.SaveChanges();
-            return View();
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the Name.");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id==null || id == 0)
+            { 
+                return NotFound();
+             }
+            Category? categoryFromDb = _db.Categories.Find(id);
+           // Category? categoryFromDb1 = _db.Categories.FirstOrDefault(u=>u.Id==id);
+           // Category? categoryFromDb2 = _db.Categories.Find(id);
+
+            if (categoryFromDb == null) 
+            {
+                return NotFound();
+            }
+           return View(categoryFromDb);
+        }
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("Name", "The DisplayOrder cannot exactly match the Name.");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _db.Update(obj); // Use Update instead of Add for editing
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
         }
     }
 }
